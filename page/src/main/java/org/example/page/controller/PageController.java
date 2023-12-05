c4ee665e9a0c3b53a9ad18a1b96015eab1ad2e10
pdsong <songpeidong@gmail.com>
2023-12-05 15:53:54 +0800

    两个eureka-server服务，注册到eureka，page跟product使用eureka方式调用

--- page/src/main/java/org/example/page/controller/PageController.java
+++ page/src/main/java/org/example/page/controller/PageController.java
@@ -2,11 +2,15 @@ package org.example.page.controller;
 
 import org.example.common.pojo.Products;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/page")
@@ -14,11 +18,19 @@ public class PageController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/getData/{id}")
     public Products findDataById(@PathVariable Integer id) {
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("lagou-service-product");
+        ServiceInstance instance = instances.get(0);
+        String host = instance.getHost();
+        int port = instance.getPort();
+        String url = "http://" + host + ":" + port + "/product/query/" + id;
         Products products =
-                restTemplate.getForObject("http://localhost:9000/product/query/" + id,
-                        Products.class);
+                restTemplate.getForObject(url, Products.class);
         System.out.println("从lagou-service-product获得product对象:" + products);
         return products;
     }
