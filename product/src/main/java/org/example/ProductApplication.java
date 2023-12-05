03bceaae07817c9ca2e7f4fadc626ff023417780
pdsong <songpeidong@gmail.com>
2023-12-05 15:13:13 +0800

    添加eureka-server,page和product是eureka-client

--- product/src/main/java/org/example/ProductApplication.java
+++ product/src/main/java/org/example/ProductApplication.java
@@ -4,9 +4,11 @@ package org.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("org.example.product.mapper")
+@EnableDiscoveryClient
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
